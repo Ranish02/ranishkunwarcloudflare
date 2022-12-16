7b2f0a356189a40bc10da687871c859aacbe7c98
Ranish02 <usaranish@gmail.com>
2022-12-17 00:27:58 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Board from "./Board";
+import Nav from "./Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Info from "./Info";
+import Languages from "./Languages";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='snap-y snap-mandatory h-screen w-screen overflow-visible overflow-x-hidden'>
+        <Info />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<>
+              <div className=''>
+                <Nav />
+                <Board />
+                <Info />
+                <Languages />
+              </div>
+
+            </>} />
+            <Route path="/path" element={<>
+              <Board />
+            </>} />
+
+          </Routes>
+          {/* <Nav /> */}
+        </BrowserRouter>
+      </div>
+
     </div>
   );
 }
